147db598815b4fa3e5fde81631e03eff75d1c27e
Khoa Nguyen <khoanguyen0791@gmail.com>
2018-03-18 13:48:17 -0500

    Added AOD mutator within the Experimental folder.

--- pitest/src/main/java/org/pitest/mutationtest/engine/gregor/config/Mutator.java
+++ pitest/src/main/java/org/pitest/mutationtest/engine/gregor/config/Mutator.java
@@ -54,222 +54,211 @@ import org.pitest.mutationtest.engine.gregor.mutators.experimental.RemoveIncreme
 import org.pitest.mutationtest.engine.gregor.mutators.experimental.RemoveSwitchMutator;
 import org.pitest.mutationtest.engine.gregor.mutators.experimental.SwitchMutator;
 
+import org.pitest.mutationtest.engine.gregor.mutators.experimental.AODMutator;
+
 public final class Mutator {
 
-  private static final Map<String, Iterable<MethodMutatorFactory>> MUTATORS = new LinkedHashMap<>();
+    private static final Map<String, Iterable<MethodMutatorFactory>> MUTATORS = new LinkedHashMap<>();
 
-  static {
+    static {
+        /**
+         * Set of AOD Mutators
+         */
+        add("AOD_FIRST", new AODMutator(AODMutator.MutantType.FIRST));
+        add("AOD_LAST", new AODMutator(AODMutator.MutantType.LAST));
+
+        /**
+         * Default mutator that inverts the negation of integer and floating point
+         * numbers.
+         */
+        add("INVERT_NEGS", InvertNegsMutator.INVERT_NEGS_MUTATOR);
+
+        /**
+         * Default mutator that mutates the return values of methods.
+         */
+        add("RETURN_VALS", ReturnValsMutator.RETURN_VALS_MUTATOR);
+
+        /**
+         * Optional mutator that mutates integer and floating point inline constants.
+         */
+        add("INLINE_CONSTS", new InlineConstantMutator());
+
+        /**
+         * Default mutator that mutates binary arithmetic operations.
+         */
+        add("MATH", MathMutator.MATH_MUTATOR);
+
+        /**
+         * Default mutator that removes method calls to void methods.
+         *
+         */
+        add("VOID_METHOD_CALLS", VoidMethodCallMutator.VOID_METHOD_CALL_MUTATOR);
+
+        /**
+         * Default mutator that negates conditionals.
+         */
+        add("NEGATE_CONDITIONALS", NegateConditionalsMutator.NEGATE_CONDITIONALS_MUTATOR);
+
+        /**
+         * Default mutator that replaces the relational operators with their boundary
+         * counterpart.
+         */
+        add("CONDITIONALS_BOUNDARY", ConditionalsBoundaryMutator.CONDITIONALS_BOUNDARY_MUTATOR);
+
+        /**
+         * Default mutator that mutates increments, decrements and assignment increments
+         * and decrements of local variables.
+         */
+        add("INCREMENTS", IncrementsMutator.INCREMENTS_MUTATOR);
+
+        /**
+         * Optional mutator that removes local variable increments.
+         */
+
+        add("REMOVE_INCREMENTS", RemoveIncrementsMutator.REMOVE_INCREMENTS_MUTATOR);
+
+        /**
+         * Optional mutator that removes method calls to non void methods.
+         */
+        add("NON_VOID_METHOD_CALLS", NonVoidMethodCallMutator.NON_VOID_METHOD_CALL_MUTATOR);
+
+        /**
+         * Optional mutator that replaces constructor calls with null values.
+         */
+        add("CONSTRUCTOR_CALLS", ConstructorCallMutator.CONSTRUCTOR_CALL_MUTATOR);
+
+        /**
+         * Removes conditional statements so that guarded statements always execute The
+         * EQUAL version ignores LT,LE,GT,GE, which is the default behaviour, ORDER
+         * version mutates only those.
+         */
+
+        add("REMOVE_CONDITIONALS_EQ_IF", new RemoveConditionalMutator(Choice.EQUAL, true));
+        add("REMOVE_CONDITIONALS_EQ_ELSE", new RemoveConditionalMutator(Choice.EQUAL, false));
+        add("REMOVE_CONDITIONALS_ORD_IF", new RemoveConditionalMutator(Choice.ORDER, true));
+        add("REMOVE_CONDITIONALS_ORD_ELSE", new RemoveConditionalMutator(Choice.ORDER, false));
+        addGroup("REMOVE_CONDITIONALS", RemoveConditionalMutator.makeMutators());
+
+        add("TRUE_RETURNS", BooleanTrueReturnValsMutator.BOOLEAN_TRUE_RETURN);
+        add("FALSE_RETURNS", BooleanFalseReturnValsMutator.BOOLEAN_FALSE_RETURN);
+        add("PRIMITIVE_RETURNS", PrimitiveReturnsMutator.PRIMITIVE_RETURN_VALS_MUTATOR);
+        add("EMPTY_RETURNS", EmptyObjectReturnValsMutator.EMPTY_RETURN_VALUES);
+        add("NULL_RETURNS", NullReturnValsMutator.NULL_RETURN_VALUES);
+        addGroup("RETURNS", betterReturns());
+
+        /**
+         * Experimental mutator that removed assignments to member variables.
+         */
+        add("EXPERIMENTAL_MEMBER_VARIABLE",
+                new org.pitest.mutationtest.engine.gregor.mutators.experimental.MemberVariableMutator());
+
+        /**
+         * Experimental mutator that swaps labels in switch statements
+         */
+        add("EXPERIMENTAL_SWITCH", new org.pitest.mutationtest.engine.gregor.mutators.experimental.SwitchMutator());
+
+        /**
+         * Experimental mutator that replaces method call with one of its parameters of
+         * matching type
+         */
+        add("EXPERIMENTAL_ARGUMENT_PROPAGATION", ArgumentPropagationMutator.ARGUMENT_PROPAGATION_MUTATOR);
+
+        /**
+         * Experimental mutator that replaces method call with this
+         */
+        add("EXPERIMENTAL_NAKED_RECEIVER", NakedReceiverMutator.NAKED_RECEIVER);
+
+        addGroup("REMOVE_SWITCH", RemoveSwitchMutator.makeMutators());
+        addGroup("DEFAULTS", defaults());
+        addGroup("STRONGER", stronger());
+        addGroup("ALL", all());
+        addGroup("NEW_DEFAULTS", newDefaults());
+        addGroup("AOD", aod());
+    }
+
+    public static Collection<MethodMutatorFactory> all() {
+        return fromStrings(MUTATORS.keySet());
+    }
+
+    private static Collection<MethodMutatorFactory> stronger() {
+        return combine(defaults(), group(new RemoveConditionalMutator(Choice.EQUAL, false), new SwitchMutator()));
+    }
+
+    private static Collection<MethodMutatorFactory> combine(Collection<MethodMutatorFactory> a,
+            Collection<MethodMutatorFactory> b) {
+        final List<MethodMutatorFactory> l = new ArrayList<>(a);
+        l.addAll(b);
+        return l;
+    }
 
     /**
-     * Default mutator that inverts the negation of integer and floating point
-     * numbers.
+     * Default set of mutators - designed to provide balance between strength and
+     * performance
      */
-    add("INVERT_NEGS", InvertNegsMutator.INVERT_NEGS_MUTATOR);
+    public static Collection<MethodMutatorFactory> defaults() {
+        return group(InvertNegsMutator.INVERT_NEGS_MUTATOR, ReturnValsMutator.RETURN_VALS_MUTATOR,
+                MathMutator.MATH_MUTATOR, VoidMethodCallMutator.VOID_METHOD_CALL_MUTATOR,
+                NegateConditionalsMutator.NEGATE_CONDITIONALS_MUTATOR,
+                ConditionalsBoundaryMutator.CONDITIONALS_BOUNDARY_MUTATOR, IncrementsMutator.INCREMENTS_MUTATOR);
+    }
+
+    public static Collection<MethodMutatorFactory> aod() {
+        return group(new AODMutator(AODMutator.MutantType.FIRST), new AODMutator(AODMutator.MutantType.LAST));
+    }
 
     /**
-     * Default mutator that mutates the return values of methods.
+     * Proposed new defaults - replaced the RETURN_VALS mutator with the new more
+     * stable set
      */
-    add("RETURN_VALS", ReturnValsMutator.RETURN_VALS_MUTATOR);
+    public static Collection<MethodMutatorFactory> newDefaults() {
+        return combine(group(InvertNegsMutator.INVERT_NEGS_MUTATOR, MathMutator.MATH_MUTATOR,
+                VoidMethodCallMutator.VOID_METHOD_CALL_MUTATOR, NegateConditionalsMutator.NEGATE_CONDITIONALS_MUTATOR,
+                ConditionalsBoundaryMutator.CONDITIONALS_BOUNDARY_MUTATOR, IncrementsMutator.INCREMENTS_MUTATOR),
+                betterReturns());
+    }
 
-    /**
-     * Optional mutator that mutates integer and floating point inline
-     * constants.
-     */
-    add("INLINE_CONSTS", new InlineConstantMutator());
+    public static Collection<MethodMutatorFactory> betterReturns() {
+        return group(BooleanTrueReturnValsMutator.BOOLEAN_TRUE_RETURN,
+                BooleanFalseReturnValsMutator.BOOLEAN_FALSE_RETURN,
+                PrimitiveReturnsMutator.PRIMITIVE_RETURN_VALS_MUTATOR, EmptyObjectReturnValsMutator.EMPTY_RETURN_VALUES,
+                NullReturnValsMutator.NULL_RETURN_VALUES);
+    }
 
-    /**
-     * Default mutator that mutates binary arithmetic operations.
-     */
-    add("MATH", MathMutator.MATH_MUTATOR);
+    private static Collection<MethodMutatorFactory> group(final MethodMutatorFactory... ms) {
+        return Arrays.asList(ms);
+    }
 
-    /**
-     * Default mutator that removes method calls to void methods.
-     *
-     */
-    add("VOID_METHOD_CALLS", VoidMethodCallMutator.VOID_METHOD_CALL_MUTATOR);
+    public static Collection<MethodMutatorFactory> byName(final String name) {
+        return FCollection.map(MUTATORS.get(name), Prelude.id(MethodMutatorFactory.class));
+    }
 
-    /**
-     * Default mutator that negates conditionals.
-     */
-    add("NEGATE_CONDITIONALS",
-        NegateConditionalsMutator.NEGATE_CONDITIONALS_MUTATOR);
+    private static void add(final String key, final MethodMutatorFactory value) {
+        MUTATORS.put(key, Collections.singleton(value));
+    }
 
-    /**
-     * Default mutator that replaces the relational operators with their
-     * boundary counterpart.
-     */
-    add("CONDITIONALS_BOUNDARY",
-        ConditionalsBoundaryMutator.CONDITIONALS_BOUNDARY_MUTATOR);
+    private static void addGroup(final String key, final Iterable<MethodMutatorFactory> value) {
+        MUTATORS.put(key, value);
+    }
 
-    /**
-     * Default mutator that mutates increments, decrements and assignment
-     * increments and decrements of local variables.
-     */
-    add("INCREMENTS", IncrementsMutator.INCREMENTS_MUTATOR);
+    public static Collection<MethodMutatorFactory> fromStrings(final Collection<String> names) {
+        final Set<MethodMutatorFactory> unique = new TreeSet<>(compareId());
 
-    /**
-     * Optional mutator that removes local variable increments.
-     */
+        FCollection.flatMapTo(names, fromString(), unique);
+        return unique;
+    }
 
-    add("REMOVE_INCREMENTS", RemoveIncrementsMutator.REMOVE_INCREMENTS_MUTATOR);
+    private static Comparator<? super MethodMutatorFactory> compareId() {
+        return (o1, o2) -> o1.getGloballyUniqueId().compareTo(o2.getGloballyUniqueId());
+    }
 
-    /**
-     * Optional mutator that removes method calls to non void methods.
-     */
-    add("NON_VOID_METHOD_CALLS",
-        NonVoidMethodCallMutator.NON_VOID_METHOD_CALL_MUTATOR);
-
-    /**
-     * Optional mutator that replaces constructor calls with null values.
-     */
-    add("CONSTRUCTOR_CALLS", ConstructorCallMutator.CONSTRUCTOR_CALL_MUTATOR);
-
-    /**
-     * Removes conditional statements so that guarded statements always execute
-     * The EQUAL version ignores LT,LE,GT,GE, which is the default behaviour,
-     * ORDER version mutates only those.
-     */
-
-    add("REMOVE_CONDITIONALS_EQ_IF", new RemoveConditionalMutator(Choice.EQUAL,
-        true));
-    add("REMOVE_CONDITIONALS_EQ_ELSE", new RemoveConditionalMutator(
-        Choice.EQUAL, false));
-    add("REMOVE_CONDITIONALS_ORD_IF", new RemoveConditionalMutator(
-        Choice.ORDER, true));
-    add("REMOVE_CONDITIONALS_ORD_ELSE", new RemoveConditionalMutator(
-        Choice.ORDER, false));
-    addGroup("REMOVE_CONDITIONALS", RemoveConditionalMutator.makeMutators());
-
-    add("TRUE_RETURNS", BooleanTrueReturnValsMutator.BOOLEAN_TRUE_RETURN);
-    add("FALSE_RETURNS", BooleanFalseReturnValsMutator.BOOLEAN_FALSE_RETURN);
-    add("PRIMITIVE_RETURNS", PrimitiveReturnsMutator.PRIMITIVE_RETURN_VALS_MUTATOR);
-    add("EMPTY_RETURNS", EmptyObjectReturnValsMutator.EMPTY_RETURN_VALUES);
-    add("NULL_RETURNS", NullReturnValsMutator.NULL_RETURN_VALUES);
-    addGroup("RETURNS", betterReturns());
-
-    /**
-     * Experimental mutator that removed assignments to member variables.
-     */
-    add("EXPERIMENTAL_MEMBER_VARIABLE",
-        new org.pitest.mutationtest.engine.gregor.mutators.experimental.MemberVariableMutator());
-
-    /**
-     * Experimental mutator that swaps labels in switch statements
-     */
-    add("EXPERIMENTAL_SWITCH",
-        new org.pitest.mutationtest.engine.gregor.mutators.experimental.SwitchMutator());
-
-    /**
-     * Experimental mutator that replaces method call with one of its parameters
-     * of matching type
-     */
-    add("EXPERIMENTAL_ARGUMENT_PROPAGATION",
-        ArgumentPropagationMutator.ARGUMENT_PROPAGATION_MUTATOR);
-
-    /**
-     * Experimental mutator that replaces method call with this
-     */
-    add("EXPERIMENTAL_NAKED_RECEIVER", NakedReceiverMutator.NAKED_RECEIVER);
-
-    addGroup("REMOVE_SWITCH", RemoveSwitchMutator.makeMutators());
-    addGroup("DEFAULTS", defaults());
-    addGroup("STRONGER", stronger());
-    addGroup("ALL", all());
-    addGroup("NEW_DEFAULTS", newDefaults());
-  }
-
-  public static Collection<MethodMutatorFactory> all() {
-    return fromStrings(MUTATORS.keySet());
-  }
-
-  private static Collection<MethodMutatorFactory> stronger() {
-    return combine(
-        defaults(),
-        group(new RemoveConditionalMutator(Choice.EQUAL, false),
-            new SwitchMutator()));
-  }
-
-  private static Collection<MethodMutatorFactory> combine(
-      Collection<MethodMutatorFactory> a, Collection<MethodMutatorFactory> b) {
-    final List<MethodMutatorFactory> l = new ArrayList<>(a);
-    l.addAll(b);
-    return l;
-  }
-
-  /**
-   * Default set of mutators - designed to provide balance between strength and
-   * performance
-   */
-  public static Collection<MethodMutatorFactory> defaults() {
-    return group(InvertNegsMutator.INVERT_NEGS_MUTATOR,
-        ReturnValsMutator.RETURN_VALS_MUTATOR, MathMutator.MATH_MUTATOR,
-        VoidMethodCallMutator.VOID_METHOD_CALL_MUTATOR,
-        NegateConditionalsMutator.NEGATE_CONDITIONALS_MUTATOR,
-        ConditionalsBoundaryMutator.CONDITIONALS_BOUNDARY_MUTATOR,
-        IncrementsMutator.INCREMENTS_MUTATOR);
-  }
-
-  /**
-   * Proposed new defaults - replaced the RETURN_VALS mutator with the new more stable set
-   */
-  public static Collection<MethodMutatorFactory> newDefaults() {
-    return combine(group(InvertNegsMutator.INVERT_NEGS_MUTATOR,
-        MathMutator.MATH_MUTATOR,
-        VoidMethodCallMutator.VOID_METHOD_CALL_MUTATOR,
-        NegateConditionalsMutator.NEGATE_CONDITIONALS_MUTATOR,
-        ConditionalsBoundaryMutator.CONDITIONALS_BOUNDARY_MUTATOR,
-        IncrementsMutator.INCREMENTS_MUTATOR), betterReturns());
-  }
-
-
-  public static Collection<MethodMutatorFactory> betterReturns() {
-    return group(BooleanTrueReturnValsMutator.BOOLEAN_TRUE_RETURN,
-        BooleanFalseReturnValsMutator.BOOLEAN_FALSE_RETURN,
-        PrimitiveReturnsMutator.PRIMITIVE_RETURN_VALS_MUTATOR,
-        EmptyObjectReturnValsMutator.EMPTY_RETURN_VALUES,
-        NullReturnValsMutator.NULL_RETURN_VALUES);
-  }
-
-  private static Collection<MethodMutatorFactory> group(
-      final MethodMutatorFactory... ms) {
-    return Arrays.asList(ms);
-  }
-
-  public static Collection<MethodMutatorFactory> byName(final String name) {
-    return FCollection.map(MUTATORS.get(name),
-        Prelude.id(MethodMutatorFactory.class));
-  }
-
-  private static void add(final String key, final MethodMutatorFactory value) {
-    MUTATORS.put(key, Collections.singleton(value));
-  }
-
-  private static void addGroup(final String key,
-      final Iterable<MethodMutatorFactory> value) {
-    MUTATORS.put(key, value);
-  }
-
-  public static Collection<MethodMutatorFactory> fromStrings(
-      final Collection<String> names) {
-    final Set<MethodMutatorFactory> unique = new TreeSet<>(
-        compareId());
-
-    FCollection.flatMapTo(names, fromString(), unique);
-    return unique;
-  }
-
-  private static Comparator<? super MethodMutatorFactory> compareId() {
-    return (o1, o2) -> o1.getGloballyUniqueId().compareTo(o2.getGloballyUniqueId());
-  }
-
-  private static Function<String, Iterable<MethodMutatorFactory>> fromString() {
-    return a -> {
-      final Iterable<MethodMutatorFactory> i = MUTATORS.get(a);
-      if (i == null) {
-        throw new PitHelpError(Help.UNKNOWN_MUTATOR, a);
-      }
-      return i;
-    };
-  }
+    private static Function<String, Iterable<MethodMutatorFactory>> fromString() {
+        return a -> {
+            final Iterable<MethodMutatorFactory> i = MUTATORS.get(a);
+            if (i == null) {
+                throw new PitHelpError(Help.UNKNOWN_MUTATOR, a);
+            }
+            return i;
+        };
+    }
 
 }
