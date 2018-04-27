63c9ac7311a5edef7feec9812bd06e218ff34ad0
Khoa T. Nguyen <khoanguyen0791@gmail.com>
2018-04-27 17:26:23 -0500

    Added M2 mutation, have NullPointerException

--- pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/augmentation/CallAnotherOverloadingMethod.java
+++ pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/augmentation/CallAnotherOverloadingMethod.java
@@ -81,7 +81,7 @@ class ReplaceWithOverloadingMethod extends AbstractInsnMutator {
 
         // don't deal with INVOKESPECIAL, INVOKEDYNAMIC right now.
 
-        if (opcode == Opcodes.INVOKEINTERFACE || opcode == Opcodes.INVOKEVIRTUAL || opcode == Opcodes.INVOKESTATIC) {
+        if (opcode == Opcodes.INVOKEVIRTUAL || opcode == Opcodes.INVOKESTATIC) {
             final MutationIdentifier muID = this.context.registerMutation(factory,
                     "Replaced with overloading method here.");
 
@@ -113,6 +113,7 @@ class ReplaceWithOverloadingMethod extends AbstractInsnMutator {
 
         // implement scan in an MV inside CV here
         ScanClassAdapter cv = new ScanClassAdapter(cw, name);
+        cv.setOldDescriptor(desc);
         cr.accept(cv, 0);
         descriptorList = cv.getMethodDescriptorList();
         accessTypeList = cv.getAccessTypeList();
@@ -137,37 +138,116 @@ class ReplaceWithOverloadingMethod extends AbstractInsnMutator {
      *            method access flag. Usually 0??? Not sure.
      */
     private void replaceMethodDescriptorMutation(int opcode, String owner, String name, String desc, boolean itf) {
+        if (descriptorList.size() > 1) {
+            String descReturnType = extractReturnType(desc);
 
-        String descReturnType = extractReturnType(desc);
+            Random rand = new Random();
 
-        Random rand = new Random();
+            int index = descriptorList.indexOf(desc);
+            n = rand.nextInt(descriptorList.size()) + 0;
 
-        int index = descriptorList.indexOf(desc);
-        n = rand.nextInt(descriptorList.size()) + 0;
-
-        while (n == index)
-        // if random selection is the current variable
-        {
-            n = rand.nextInt(descriptorList.size()) + 0;// select a new variable at random
-        }
-
-        final MutationIdentifier newId = this.context.registerMutation(this.factory,
-                "Replaced " + descriptorList.get(index) + " with " + descriptorList.get(n));
-        if (this.context.shouldMutate(newId)) {
-            // find index of replacment var in our list, and use
-            // that index to find index of the replacment variable
-            // in localvar table
-
-            int newOpcode = opcode;
-            String newOwner = owner;
-            String newName = name;
-            String newDesc = descriptorList.get(n);
-
-            if ((accessTypeList.get(n) & Opcodes.ACC_STATIC) != 0) {
-                newOpcode = Opcodes.INVOKESTATIC;
+            while (n == index)
+            // if random selection is the current variable
+            {
+                n = rand.nextInt(descriptorList.size()) + 0;// select a new variable at random
             }
 
-            super.visitMethodInsn(newOpcode, newOwner, newName, newDesc, itf);
+            final MutationIdentifier newId = this.context.registerMutation(this.factory,
+                    "Replaced " + descriptorList.get(index) + " with " + descriptorList.get(n));
+            if (this.context.shouldMutate(newId)) {
+                // find index of replacment var in our list, and use
+                // that index to find index of the replacment variable
+                // in local var table
+
+                int newOpcode = opcode;
+                String newOwner = owner;
+                String newName = name;
+                String newDesc = descriptorList.get(n);
+
+                if ((accessTypeList.get(n) & Opcodes.ACC_STATIC) != 0) {
+                    newOpcode = Opcodes.INVOKESTATIC;
+                }
+
+                this.replaceStack(desc, newDesc);
+                // manipulate the stack here
+                super.visitMethodInsn(newOpcode, newOwner, newName, newDesc, itf);
+            }
+        } else {
+            super.visitMethodInsn(opcode, owner, name, desc, itf);
+        }
+    }
+
+    /**
+     * Relace the old stack with a new stack. It is guaranteed that the old stack
+     * contains the new stack OR the new stack contains the old stack
+     * 
+     * @param oldDesc
+     * @param newDesc
+     */
+    public void replaceStack(String oldDesc, String newDesc) {
+        if (newContainsOld(oldDesc,newDesc)) { // newdesc is longer than old
+            int numOfPops = newDesc.length() - oldDesc.length();
+            newDesc = newDesc.substring(1, newDesc.length() - 2);
+            int index = newDesc.length() - 1;
+            for (int i = 0; i < numOfPops; i++) {
+                pushValues(""+newDesc.toCharArray()[index - i]);
+            }
+        } else if (oldContainsNew(oldDesc,newDesc)) { // olddesc is longer than new
+            int numOfPops = oldDesc.length() - newDesc.length();
+            oldDesc = oldDesc.substring(1, oldDesc.length() - 2);
+            int index = oldDesc.length() - 1;
+            for (int i = 0; i < numOfPops; i++) {
+                popValues(""+oldDesc.toCharArray()[index - i]);
+            }
+        }
+    }
+
+    public boolean oldContainsNew(String oldDesc, String newDesc) {
+
+        // add a method to check if old descriptor contains new descriptor: (III)I -> (I
+        // or (II
+        return oldDesc.length() > newDesc.length();
+
+    }
+
+    public boolean newContainsOld(String oldDesc, String newDesc) {
+
+        // add a method to check if new descriptor contains old descriptor
+        // old: (ID)I
+        // new: (IDD)I
+
+        return oldDesc.length() < newDesc.length();
+    }
+
+    /**
+     * Pop parameters from the stack
+     * 
+     * @param param
+     */
+    public void popValues(String param) {
+        if (param.equalsIgnoreCase("I") || param.equalsIgnoreCase("F")) {
+            super.visitInsn(Opcodes.POP);
+        } else if (param.equalsIgnoreCase("D") || param.equalsIgnoreCase("J")) {
+            super.visitInsn(Opcodes.POP2);
+        } else {
+            super.visitInsn(Opcodes.POP);
+        }
+    }
+
+    /**
+     * Come up with a default value of a certain type and push it onto the stack
+     */
+    public void pushValues(String param) {
+        if (param.equalsIgnoreCase("I")) {
+            super.visitInsn(Opcodes.ICONST_0);
+        } else if (param.equalsIgnoreCase("D")) {
+            super.visitInsn(Opcodes.DCONST_0);
+        } else if (param.equalsIgnoreCase("F")) {
+            super.visitInsn(Opcodes.FCONST_0);
+        } else if (param.equalsIgnoreCase("J")) {
+            super.visitInsn(Opcodes.LCONST_0);
+        } else {
+            super.visitInsn(Opcodes.ACONST_NULL);
         }
     }
 
--- pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/augmentation/ScanClassAdapter.java
+++ pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/augmentation/ScanClassAdapter.java
@@ -16,10 +16,16 @@ import org.objectweb.asm.MethodVisitor;
 public class ScanClassAdapter extends ClassVisitor {
 
     private static String methodNameToScan;
+
     private String returnTypeToScan = methodNameToScan.substring(methodNameToScan.indexOf(")"));
     private List<String> methodDescriptorList = new ArrayList<String>();
     private List<Integer> accessTypeList = new ArrayList<Integer>();
     private List<Boolean> staticAccessList = new ArrayList<Boolean>();
+    private String oldDescriptor;
+
+    public void setOldDescriptor(String oldDescriptor) {
+        this.oldDescriptor = oldDescriptor;
+    }
 
     public ScanClassAdapter(ClassVisitor cv, String methodNameToScan) {
         super(Opcodes.ASM6, cv);
@@ -35,24 +41,55 @@ public class ScanClassAdapter extends ClassVisitor {
     @Override
     public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
         boolean isSynthetic = (access & Opcodes.ACC_SYNTHETIC) != 0;
+        boolean isPublic = (access & Opcodes.ACC_PUBLIC) != 0;
+        boolean isPrivate = (access & Opcodes.ACC_PRIVATE) != 0;
+        boolean isProtected = (access & Opcodes.ACC_PROTECTED) != 0;
+        boolean isStatic = (access & Opcodes.ACC_STATIC) != 0;
         String descReturnType = desc.substring(desc.indexOf(")"));
 
-        if (name.equalsIgnoreCase(methodNameToScan) && !isSynthetic
+        // if the method name is the same, method is not synthetic and it is public, the
+        // return type is the same
+        if (name.equalsIgnoreCase(methodNameToScan) && !isSynthetic && isPublic
                 && descReturnType.equalsIgnoreCase(returnTypeToScan)) {
-            // check method access: public, private or protected. Static method might cause
-            // a problem.
-            // Some method access is: ACC_PUBLIC + ACC_STATIC
-            boolean isPublic = (access & Opcodes.ACC_PUBLIC) != 0;
-            boolean isPrivate = (access & Opcodes.ACC_PRIVATE) != 0;
-            boolean isProtected = (access & Opcodes.ACC_PROTECTED) != 0;
-            boolean isStatic = (access & Opcodes.ACC_STATIC) != 0;
 
-            methodDescriptorList.add(desc);
-            accessTypeList.add(access);
-            if (isStatic) {
-                staticAccessList.add(true);
-            } else {
-                staticAccessList.add(false);
+            // If one descriptor contains the other descriptor (has similar parameters
+            ArrayList<Character> oldArgs = new ArrayList<Character>();
+            ArrayList<Character> newArgs = new ArrayList<Character>();
+            for (int i = 0; i < oldDescriptor.length(); i++) {
+                oldArgs.add(oldDescriptor.charAt(i));
+            }
+            oldArgs.remove(oldArgs.size() - 1);
+            oldArgs.remove(oldArgs.size() - 1);
+            oldArgs.remove(0);
+
+            for (int i = 0; i < desc.length(); i++) {
+                newArgs.add(desc.charAt(i));
+            }
+            newArgs.remove(newArgs.size() - 1);
+            newArgs.remove(newArgs.size() - 1);
+            newArgs.remove(0);
+            int minLength = Math.min(oldArgs.size(), newArgs.size());
+            boolean sameArgs = true;
+
+            for (int i = 0; i < minLength; i++) {
+                if (!(oldArgs.get(i).equals(newArgs.get(i)))) {
+                    sameArgs = false;
+                    break;
+                }
+            }
+
+            if (sameArgs) {
+                // check method access: public, private or protected. Static method might cause
+                // a problem.
+                // Some method access is: ACC_PUBLIC + ACC_STATIC
+
+                methodDescriptorList.add(desc);
+                accessTypeList.add(access);
+                if (isStatic) {
+                    staticAccessList.add(true);
+                } else {
+                    staticAccessList.add(false);
+                }
             }
         }
 
@@ -70,4 +107,5 @@ public class ScanClassAdapter extends ClassVisitor {
     public List<Integer> getAccessTypeList() {
         return accessTypeList;
     }
+
 }
